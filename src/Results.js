79b0e79ef44a8e9c8d2be9208a1d6fe2c153d1ae
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-09-27 17:17:41 +0700

    added results body
    - changed class tags to className

---
+++ src/Results.js
@@ -0,0 +1,58 @@
+import React from 'react'
+class Results extends React.Component {
+	render() {
+		return (<div className="results">
+      <small>37 Results</small>
+      <small>www.w3school.com</small>
+    </div>
+    <section>
+      <a href="#" target="_blank" className="result-title"
+        ><h1>JavaScript Tutorial - W3School</h1></a
+      >
+      <p>
+        Well organized and easy to understand Web building tutorials with lots
+        of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python,
+        Bootstrap, Java
+      </p>
+      <ul className="links">
+        <li><a href="#" target="_blank">JavaScript Introduction</a></li>
+        <li><a href="#" target="_blank">JS Functions</a></li>
+        <li><a href="#" target="_blank">JavaScript Examples</a></li>
+      </ul>
+      <small>developer.mozilla.org</small>
+    </section>
+    <section className="search-results">
+      <a href="#" target="_blank" class="result-title"
+        ><h1>JavaScript | MDN</h1></a
+      >
+
+      <p>
+        JavaScript (JS) is a lightweight, interpreted, or just-in-time complied
+        programming language with first-class functions. While it is most
+        well-known as the scrpiting language for Web pages, CouchDB and Adobe
+        Acrobat.
+      </p>
+
+      <ul className="links" className="search-results">
+        <li><a href="#" target="_blank">JavaScript Operator</a></li>
+        <li><a href="#" target="_blank">JS Code</a></li>
+        <li><a href="#" target="_blank">JavaScript Meaning</a></li>
+      </ul>
+      <small>en.wikipedia.org</small>
+    </section>
+    <section className="search-results">
+      <a href="#" target="_blank" className="result-title"
+        ><h1>JavaScript - Wikipedia</h1></a
+      >
+      <p>
+        JavaScript often abbreviated as JS, is a programming language that
+        conforms to ECMAScript specification. JavaScript is high-level, often
+        just-in-time.
+      </p>
+      <ul className="links">
+        <li><a href="#" target="_blank">JavaScript Online</a></li>
+        <li><a href="#" target="_blank">JavaScript Definition</a></li>
+      </ul>
+    </section>)
+	}
+}
