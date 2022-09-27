d787020e17208cc94d6aab4df7fb5d2f28b57fb2
Joshua Peoples <46235676+jpcoder12@users.noreply.github.com>
2022-09-27 17:07:40 +0700

    Rendering search page
    - Moved css to index.css
    - Added component for search page
    - adjustments to app rendering from original project

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React from 'react'
 class App extends React.Component {
     render(){
         return(
-            <div class="wrap">
-                <img src="images/google.png" className="logo" />
+            <div className="wrap">
+                <img alt='Google Logo' src="images/google.png" className="logo" />
                 <form>
                     <input type="search" name="search" />
-                    <button type="submit">Search</button>
+                    <button>Search</button>
                 </form>
             </div>
         )
