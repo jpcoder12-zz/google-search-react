9e44d3fd85e1a6dd43ce4ef8c72038b3008e7705
Joshua Peoples <46235676+jpcoder12@users.noreply.github.com>
2022-09-27 16:47:55 +0700

    Added app component for Search
    Added HTML content to app.js for rendering.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-<div class="wrap">
-   <img src="images/google.png" class="logo" />
-    <form>
-        <input type="search" name="search" />
-        <button type="submit">Search</button>
-    </form>
+import React from 'react'
+
+class App extends React.Component {
+    render(){
+        return(
+            <div class="wrap">
+                <img src="images/google.png" className="logo" />
+                <form>
+                    <input type="search" name="search" />
+                    <button type="submit">Search</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default App
