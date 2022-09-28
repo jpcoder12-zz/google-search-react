ed9451f5a0c2db692c1ca900cf03e85724ab9ea4
Joshua Peoples <46235676+jpcoder12@users.noreply.github.com>
2022-09-28 15:34:51 +0700

    Switch focus to Results page
    Changed index so the project renders the results page. Changed class to className to follow JSX syntax.

--- src/Results.js
+++ src/Results.js
@@ -3,22 +3,23 @@ class Results extends React.Component {
 	render() {
 		return (
 			<>
-			<nav class="wrap-results">
-      <img src="images/google.png" class="logo-results" />
-      <form class="header-results">
+			<nav className="wrap-results">
+      <img  alt='' src="images/google.png" className="logo-results" />
+      <form className="header-results">
         <input type="search" />
         <button type="submit">
           Search
         </button>
       </form>
     </nav>
-    <div class="results">
-      <small>37 Results</small>
+    
+    <div className="results">
+    <small>37 Results</small>
       <small>www.w3school.com</small>
     </div>
 			<div>
     <section>
-      <a href="#" target="_blank" className="result-title"
+      <a href="/" target="_blank" className="result-title"
         ><h1>JavaScript Tutorial - W3School</h1></a
       >
       <p>
@@ -27,14 +28,14 @@ class Results extends React.Component {
         Bootstrap, Java
       </p>
       <ul className="links">
-        <li><a href="#" target="_blank">JavaScript Introduction</a></li>
-        <li><a href="#" target="_blank">JS Functions</a></li>
-        <li><a href="#" target="_blank">JavaScript Examples</a></li>
+        <li><a href="/" target="_blank">JavaScript Introduction</a></li>
+        <li><a href="/" target="_blank">JS Functions</a></li>
+        <li><a href="/" target="_blank">JavaScript Examples</a></li>
       </ul>
       <small>developer.mozilla.org</small>
     </section>
     <section className="search-results">
-      <a href="#" target="_blank" class="result-title"
+      <a href="/" target="_blank" className="result-title"
         ><h1>JavaScript | MDN</h1></a
       >
 
@@ -46,14 +47,14 @@ class Results extends React.Component {
       </p>
 
       <ul className="links" className="search-results">
-        <li><a href="#" target="_blank">JavaScript Operator</a></li>
-        <li><a href="#" target="_blank">JS Code</a></li>
-        <li><a href="#" target="_blank">JavaScript Meaning</a></li>
+        <li><a href="/" target="_blank">JavaScript Operator</a></li>
+        <li><a href="/" target="_blank">JS Code</a></li>
+        <li><a href="/" target="_blank">JavaScript Meaning</a></li>
       </ul>
       <small>en.wikipedia.org</small>
     </section>
     <section className="search-results">
-      <a href="#" target="_blank" className="result-title"
+      <a href="/" target="_blank" className="result-title"
         ><h1>JavaScript - Wikipedia</h1></a
       >
       <p>
@@ -62,13 +63,11 @@ class Results extends React.Component {
         just-in-time.
       </p>
       <ul className="links">
-        <li><a href="#" target="_blank">JavaScript Online</a></li>
-        <li><a href="#" target="_blank">JavaScript Definition</a></li>
+        <li><a href="/" target="_blank">JavaScript Online</a></li>
+        <li><a href="/" target="_blank">JavaScript Definition</a></li>
       </ul>
     </section>
 	</div>
-	</>
-   )
-	}
-}
+	</>)
+}}
 export default Results
