a1c2ace5f860d959b59e8446c57bc3dd27cecdc8
Joshua Peoples <46235676+jpcoder12@users.noreply.github.com>
2022-09-28 15:22:39 +0700

    Change App.js to Search.js
    Renamed App.js file to Search and adjusted Index.js accordingly to show that the component works.

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-class App extends React.Component {
+class Search extends React.Component {
     render(){
         return(
             <div className="wrap">
@@ -14,4 +14,4 @@ class App extends React.Component {
     }
 }
 
-export default App
+export default Search
